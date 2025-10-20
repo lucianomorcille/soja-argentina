0edd9ed9420ce9079b2e94c9669452faea2a90d9
Luciano Morcille <lucianomorcille99@gmail.com>
2025-10-20 15:58:04 -0300

    Primer commit - estructura generada con Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='El dominio se enmarca en el sector agropecuario, especificamente en el cultivo de soja en Argentina.El objetivo de este proyecto es predecir el rendimiento de soja promedio a partir de variables históricas.',
+    author='Luciano Morcille',
+    license='MIT',
+)
